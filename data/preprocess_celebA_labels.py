c8512e22c0d091119b6a27817319e67999e8bba6
amelieobrk <94685992+amelieobrk@users.noreply.github.com>
2025-05-28 15:22:24 +0200

    Update preprocess_celebA_labels.py

--- data/preprocess_celebA_labels.py
+++ data/preprocess_celebA_labels.py
@@ -1,14 +1,14 @@
 import os
 import pandas as pd
 
-# Verzeichnis mit den verarbeiteten Bildern
+#image directories
 preprocessed_dir = "/home/lab24inference/amelie/data/preprocessed_celebA"
 label_file = os.path.join(preprocessed_dir, "labels.csv")
 
-# Liste für Dateinamen und Labels
+# List for datanames and labels
 data = []
 
-# Alle Dateien im Verzeichnis durchgehen
+
 for file_name in os.listdir(preprocessed_dir):
     if file_name.endswith(".jpg"):  # Nur Bilder berücksichtigen
         if file_name.startswith("smiling"):
@@ -20,10 +20,10 @@ for file_name in os.listdir(preprocessed_dir):
 
         data.append((file_name, label))
 
-# DataFrame erstellen
+#Create Dataframe
 df = pd.DataFrame(data, columns=["filename", "label"])
 
-# CSV speichern
+#Save CSV
 df.to_csv(label_file, index=False)
 
 print(f"Labels gespeichert in {label_file}")
